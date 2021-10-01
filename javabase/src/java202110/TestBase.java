65ab5bab40cfd1aead9857df15e6131ae2994e51
schen <13563842296@163.com>
2021-10-01 19:50:20 +0800

    0

--- javabase/src/java202110/TestBase.java
+++ javabase/src/java202110/TestBase.java
@@ -2,10 +2,20 @@ package java202110;
 
 /**
  * @ClassName TestBase
- * @Description
+ * @Description 基本的语法知识
  * @Author GOFAesir
  * @Data 2021/9/30
  **/
 public class TestBase {
+    public static void main(String[] args) {
+//        short s = 5;
+//        s = (short)(s - 2);
+//        System.out.println(s);
+
+//        byte b = 5;
+//        short s = 3;
+//        short t = s + b;
+
+    }
 
 }
