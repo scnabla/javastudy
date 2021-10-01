193e6d190845f6fff09d5e670e83efebed3f6bd9
schen <13563842296@163.com>
2021-10-01 22:13:38 +0800

    0

--- javabase/src/java202110/TestBase.java
+++ javabase/src/java202110/TestBase.java
@@ -9,12 +9,64 @@ package java202110;
 public class TestBase {
     public static void main(String[] args) {
 //        short s = 5;
-//        s = (short)(s - 2);
+//        s = (short)(s - 2); 会自动类型提升，需要强制类型转换
 //        System.out.println(s);
 
 //        byte b = 5;
 //        short s = 3;
 //        short t = s + b;
+
+//        int i = 1;
+//        i *= 0.1;        不会改变i原始的变量类型
+//        System.out.println(i);
+//        i++;
+//        System.out.println(i);
+
+//        int m = 2;
+//        int n = 3;
+//        n *= m++;
+//        System.out.println("m=" + m);
+//        System.out.println("n=" + n);
+
+//        int n = 10;
+//        n += (n++) + (++n);
+//        System.out.println(n);
+
+//        int x = 1;
+//        int y=1;
+//        if(x++==2 & ++y==2){
+//            x =7;
+//        }
+//        System.out.println("x="+x+",y="+y);
+
+//        int x = 1,y = 1;
+//        if(x++==2 && ++y==2){
+//            x =7;
+//        }
+//        System.out.println("x="+x+",y="+y);
+
+//        int x = 1,y = 1;
+//        if(x++==1 | ++y==1){
+//            x =7;
+//        }
+//        System.out.println("x="+x+",y="+y);
+
+//        int x = 1,y = 1;
+//        if(x++==1 || ++y==1){
+//            x =7;
+//        }
+//        System.out.println("x="+x+",y="+y);
+
+//        boolean b = true;
+//      如果写成if(b=false)能编译通过吗？如果能，结果是？
+//        if(b = false)
+//            System.out.println("a");
+//        else if(b)
+//            System.out.println("b");
+//        else if(!b)
+//            System.out.println("c");
+//        else
+//            System.out.println("d");
 
     }
 
