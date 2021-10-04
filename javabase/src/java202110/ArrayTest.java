d7542b0e4655f933907e0adafd1432ea4f71f831
schen <13563842296@163.com>
2021-10-04 19:35:42 +0800

    0

--- javabase/src/java202110/ArrayTest.java
+++ javabase/src/java202110/ArrayTest.java
@@ -2,21 +2,27 @@ package java202110;
 
 /**
  * @ClassName ArrayTest
- * @Description
+ * @Description 类的成员，方法的重载
  * @Author GOFAesir
  * @Data 2021/10/2
  **/
 public class ArrayTest {
+    public static void main(String[] args) {
+        String[] a = new String[]{"jfsdksm","sjsnfksdkfm"};
+        test t = new test();
+        t.md("sldfjs","sjdsdklfm","sldnfsjidk");
 
+    }
 }
 
 class test{
     int i = 1;
-
-    public void md(){
-        System.out.println(i);
-    }
+//    public void md(){
+//        System.out.println(i);
+//    }
     public void md(String ... p){
-        System.out.println(i+1);
+        System.out.println("this p");
+        System.out.println(p[1]);
+        System.out.println(p);
     }
 }
