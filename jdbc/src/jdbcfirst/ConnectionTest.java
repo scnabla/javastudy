743f66fd5e2c1c6ec0d4686b78c8ec43fb37d823
schen <13563842296@163.com>
2021-10-10 23:06:08 +0800

    0

--- jdbc/src/jdbcfirst/ConnectionTest.java
+++ jdbc/src/jdbcfirst/ConnectionTest.java
@@ -1,5 +1,11 @@
 package jdbcfirst;
 
+import org.junit.Test;
+
+import java.sql.*;
+import java.util.Properties;
+import java.util.logging.Logger;
+
 /**
  * @ClassName ConnectionTest
  * @Description TODO
@@ -7,4 +13,45 @@ package jdbcfirst;
  * @Data 2021/10/10
  **/
 public class ConnectionTest {
+    @Test
+    public void testConnection1(){
+        Driver driver = new Driver() {
+            @Override
+            public Connection connect(String s, Properties properties) throws SQLException {
+                return null;
+            }
+
+            @Override
+            public boolean acceptsURL(String s) throws SQLException {
+                return false;
+            }
+
+            @Override
+            public DriverPropertyInfo[] getPropertyInfo(String s, Properties properties) throws SQLException {
+                return new DriverPropertyInfo[0];
+            }
+
+            @Override
+            public int getMajorVersion() {
+                return 0;
+            }
+
+            @Override
+            public int getMinorVersion() {
+                return 0;
+            }
+
+            @Override
+            public boolean jdbcCompliant() {
+                return false;
+            }
+
+            @Override
+            public Logger getParentLogger() throws SQLFeatureNotSupportedException {
+                return null;
+            }
+        }
+
+
+    }
 }
